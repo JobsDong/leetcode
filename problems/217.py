6a3b31b833e595918c439e4e662f7d92642275e0
JobsDong <wuyadong@shuimuhh.com>
2016-08-05 15:46:54 +0800

    problem 217

---
+++ problems/217.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+__author__ = ['"wuyadong" <wuyadong311521@gmail.com>']
+
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        s = set()
+        for num in nums:
+            if num in s:
+                return True
+            else:
+                s.add(num)
+        return False
